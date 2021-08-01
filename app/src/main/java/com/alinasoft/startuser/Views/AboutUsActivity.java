197fa4f3c0e1b810691c65f7f36880ed3d8d1151
Vitalie Bancu <vitalie.bancu@statistica.gov.md>
2021-08-01 13:10:45 +0300

    Fixed some bugs

--- app/src/main/java/com/alinasoft/startuser/Views/AboutUsActivity.java
+++ app/src/main/java/com/alinasoft/startuser/Views/AboutUsActivity.java
@@ -1,4 +1,5 @@
 package com.alinasoft.startuser.Views;
+import android.content.Intent;
 import android.os.Bundle;
 //import androidx.appcompat.widget.Toolbar;
 import com.alinasoft.startuser.R;
@@ -13,5 +14,14 @@ public class AboutUsActivity extends BaseActivity{
       //  Toolbar toolbar = findViewById(R.id.aboutUsToolBar);
 
         findViewById(R.id.mBackArrow).setOnClickListener(v ->finish());
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent;
+        intent = new Intent(this, DashboardActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        finish();
+        startActivity(intent);
     }
 }
